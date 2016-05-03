f57c2a445e759ba54094893b44fa728c45d68f38
miche.atucha <miche.atucha@gmail.com>
2016-05-02 19:03:00 -0500

    javadoc

--- kasije-main/src/main/java/com/kasije/main/Main.java
+++ kasije-main/src/main/java/com/kasije/main/Main.java
@@ -35,6 +35,7 @@ public class Main
 {
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception
     {
